1327cf8e0fd98277b70606f312b562240a6fb1db
vivanpaluskar <77099024+vivanpaluskar@users.noreply.github.com>
2022-10-02 00:35:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+objects=[];
+song = "";
+status = "";
+function preload(){
+    song = loadSound("alarm.wav");
+}
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+function draw(){
+    image(video, 0, 0, 380, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y +15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objects[i].label == "person"){
+                document.getElementById("number_of_objects").innerHTML = "Baby found";
+                console.log("stop");
+                song.stop();
+            }
+            else{
+                document.getElementById("number_of_objects").innerHTML = "Baby not found";
+                console.log("play");
+                song.play();
+            }
+        }
+        if(objects.length == 0){
+            document.getElementById("number_of_objects").innerHTML = "Baby not found";
+            console.log("play");
+            song.play();
+        }
+    }
+}
+function start(){}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
